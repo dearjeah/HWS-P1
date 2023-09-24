e4eaa08fd7bdfa4d786e158896ffc0f6bb600c48
Delvina J <j.delvina@gmail.com>
2023-09-24 22:19:16 +0700

    update: main code func

--- HWS-P1/ViewController.swift
+++ HWS-P1/ViewController.swift
@@ -2,16 +2,47 @@
 //  ViewController.swift
 //  HWS-P1
 //
-//  Created by Delvina Janice on 24/09/23.
+//  Created by dearjeah on 24/09/23.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var pictures: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.getItems()
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func getItems() {
+        let fm = FileManager.default
+        
+        // note: it's ok to use 'forced' in this case cause we know the files are there
+        // use optional if you are not sure
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
     }
 
 
